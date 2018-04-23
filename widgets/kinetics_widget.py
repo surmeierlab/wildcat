b592f5099e148611570d8e28283270a396a8dd59
Dan Galtieri <daniel.galtieri@gmail.com>
2018-04-23 10:21:53 -0500

    updated to allow for settings in fit

--- widgets/kinetics_widget.py
+++ widgets/kinetics_widget.py
@@ -26,25 +26,54 @@ class KineticsWidget(QtWidgets.QWidget):
 
         ## Parameters
         param_label = QtWidgets.QLabel('Paramaters:')
+        param_label.setSizePolicy(size_policy)
 
         # fit start
-        self.bsl_start_layout = QtWidgets.QHBoxLayout()
-        self.bsl_start_label = QtWidgets.QLabel('Baseline Start: ')
-        self.bsl_start_val = QtWidgets.QLineEdit('0.0')
-
+        self.start_layout = QtWidgets.QHBoxLayout()
+        self.start_label = QtWidgets.QLabel('Fit Start (s): ')
+        self.start_label.setFixedWidth(120)
+        self.start_val = QtWidgets.QLineEdit('0.0')
+        self.start_val.setSizePolicy(size_policy)
+        self.start_val.setFixedWidth(70)
+        self.start_layout.addWidget(self.start_label)
+        self.start_layout.addWidget(self.start_val)
 
         # fit end
+        self.end_layout = QtWidgets.QHBoxLayout()
+        self.end_label = QtWidgets.QLabel('Fit End (s): ')
+        self.end_label.setFixedWidth(120)
+        self.end_val = QtWidgets.QLineEdit()
+        self.end_val.setFixedWidth(70)
+        self.end_layout.addWidget(self.end_label)
+        self.end_layout.addWidget(self.end_val)
+
+        # peak delta
+        self.delta_layout = QtWidgets.QHBoxLayout()
+        self.delta_label = QtWidgets.QLabel('Peak Delta (# points): ')
+        self.delta_label.setFixedWidth(120)
+        self.delta_val = QtWidgets.QLineEdit('0')
+        self.delta_val.setFixedWidth(70)
+        self.delta_layout.addWidget(self.delta_label)
+        self.delta_layout.addWidget(self.delta_val)
 
         # baseline start
+        # self.bsl_start_layout = QtWidgets.QHBoxLayout()
+        # self.bsl_start_label = QtWidgets.QLabel('Baseline Start: ')
+        # self.bsl_start_val = QtWidgets.QLineEdit('0.0')
 
         # baseline end
+
+        ## Output
+        output_label = QtWidgets.QLabel('Fit Output: ')
+        output_label.setSizePolicy(size_policy)
           
         # rise tau
         self.rise_layout = QtWidgets.QHBoxLayout()
         self.rise_label = QtWidgets.QLabel('Rise tau: ')
+        self.rise_label.setFixedWidth(120)
         self.rise_val = QtWidgets.QLineEdit()
         self.rise_val.setSizePolicy(size_policy)
-        self.rise_val.setFixedWidth(100)
+        self.rise_val.setFixedWidth(70)
         self.rise_val.setReadOnly(True)
         self.rise_layout.addWidget(self.rise_label)
         self.rise_layout.addWidget(self.rise_val)
@@ -52,16 +81,23 @@ class KineticsWidget(QtWidgets.QWidget):
         # decay tau
         self.decay_layout = QtWidgets.QHBoxLayout()
         self.decay_label = QtWidgets.QLabel('Decay tau: ')
+        self.decay_label.setFixedWidth(120)
         self.decay_val = QtWidgets.QLineEdit()
         self.decay_val.setSizePolicy(size_policy)
-        self.decay_val.setFixedWidth(100)
+        self.decay_val.setFixedWidth(70)
         self.decay_val.setReadOnly(True)
         self.decay_layout.addWidget(self.decay_label)
         self.decay_layout.addWidget(self.decay_val)
 
         run_btn = QtWidgets.QPushButton('Run Fit')
         run_btn.clicked.connect(self.fit_transient)
+        run_btn.setFixedWidth(200)
 
+        left_col.addWidget(param_label)
+        left_col.addLayout(self.start_layout)
+        left_col.addLayout(self.end_layout)
+        left_col.addLayout(self.delta_layout)
+        left_col.addWidget(output_label)
         left_col.addLayout(self.rise_layout)
         left_col.addLayout(self.decay_layout)
         left_col.addWidget(run_btn)
@@ -120,8 +156,9 @@ class KineticsWidget(QtWidgets.QWidget):
         current = self.dm.ip_data
         peak_ix = np.argmax(current)
 
-        ## TODO implement rise start setting
-        rise_start = peak_ix - 5 
+        ## TODO implement exception handling
+        start_t = float(self.start_val.text())
+        rise_start= np.where(self.time >= start_t)[0][0]
 
         # rise fit
         rise_sub_time = self.time[rise_start:peak_ix+1] 
@@ -132,13 +169,14 @@ class KineticsWidget(QtWidgets.QWidget):
             rise_fit = np.full(rise_sub.size, np.nan)
             rise_popt = [np.nan, np.nan, np.nan]
 
-        # TODO implement decay start setting and fit stop
-        decay_start = 0
-        fit_stop = peak_ix + 20
+        # TODO implement exception handling
+        peak_delta = int(self.delta_val.text())
+        end_t = float(self.end_val.text())
+        fit_stop = np.where(self.time <= end_t)[0][-1]
 
         # decay fit
-        decay_sub_time = self.time[peak_ix+decay_start:fit_stop]
-        decay_sub = current[peak_ix+decay_start:fit_stop]
+        decay_sub_time = self.time[peak_ix+peak_delta:fit_stop]
+        decay_sub = current[peak_ix+peak_delta:fit_stop]
         try:
             decay_fit, decay_popt = self.fit_decay(decay_sub, decay_sub_time)
         except RuntimeError:
